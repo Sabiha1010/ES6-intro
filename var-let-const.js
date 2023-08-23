b4aba5b950fe23565f6c5214f125a00a6d85a6ee
Sabiha1010 <islam.sabiha1010@gmail.com>
2023-08-23 20:11:38 +0600

    first commit

---
+++ var-let-const.js
@@ -0,0 +1,33 @@
+// var : no reason to use var 
+// let: allow to reassign
+// cost: do not allow it to reassign
+
+
+const money = 25;
+//money = 50;
+const rich = money + 25;
+console.log(rich);
+
+let count = 0;
+count = count +10;
+console.log(count)
+
+const numbers = [23, 4, 23, 12, 56];
+numbers[1] = 55;
+numbers.push(8,9,65);
+console.log(numbers)
+// object
+const student = {
+    name: 'moyna pakhi',
+    class: 12
+}
+student.name = 'moyur konthi';
+console.log(student)
+
+//loop
+let sum = 0;
+for(let i = 0; i <10; i++){
+    sum = sum + i;
+}
+
+console.log(sum)
